e7b5d9b1aeab82b995a99cb969a34671836d653c
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-09-24 14:12:13 +0700

    image next js dan lazy load

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Layout from "./../components/Layout";
 import Header from "./../components/Header";
-
 const Home: NextPage = () => {
   return (
     <>
       <div>
         <Layout pageTitle="Home">
+          <Image src="/qwerty.jpg" width={200} height={200} />
           <p>index</p>
         </Layout>
       </div>
